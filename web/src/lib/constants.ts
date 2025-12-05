25ade52d77601427283a366313aa23548982f788
Sakshi947-s <sakshissm20@gmail.com>
2025-12-05 17:44:39 +0530

    constant

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -1,4 +1,4 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const PROGRAM_ID = new PublicKey("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS"); // Replace with actual ID after deploy
+export const PROGRAM_ID = new PublicKey("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS"); 
 export const STUDENT_TAG = "student";
